da78cce579e909a20e6203c77e00723fbff69374
Merriidiian <meridian9695@mail.ru>
2024-02-25 21:02:35 +0300

    feat: Add equip domain

---
+++ Backend/RFLOT.WebApi/RFLOT.Domain/Equip/Equip.cs
@@ -0,0 +1,15 @@
+﻿namespace RFLOT.Domain;
+
+public class Equip
+{
+    public string RfId { get; set; }
+    public Guid IdZone { get; set; }
+    public string PlanePlace { get; set; }
+    public string Name { get; set; }
+    public Type Type { get; set; }
+    public DateTimeOffset DateTimeStart { get; set; }
+    public DateTimeOffset DateTimeEnd { get; set; }
+    public Status LastStatus { get; set; }
+
+}
+
---
+++ Backend/RFLOT.WebApi/RFLOT.Domain/Equip/Status.cs
@@ -0,0 +1,22 @@
+﻿namespace RFLOT.Domain;
+
+/// <summary>
+/// 0 – Ok (Проблем нет),
+/// 1 – None (Не найден)
+/// 2 – NotFound (Не сканируется),
+/// 3 – DataFail (истёк срок годности),
+/// 4 – DataMonth (срок годности меньше месяца)
+/// </summary>
+public enum Status
+{
+    ///<summary>Проблем нет</summary>
+    Ok, 
+    ///<summary>Не найден</summary>
+    None,
+    ///<summary>Не сканируется</summary>
+    NotFound,
+    ///<summary>Истёк срок годности</summary>
+    DateFail,
+    ///<summary>Срок годности меньше месяца</summary>
+    DateMonth
+}
