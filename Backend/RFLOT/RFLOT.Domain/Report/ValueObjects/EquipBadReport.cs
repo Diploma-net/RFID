b87f929b3c0780049e637ab5c88394c70ffcd761
Merriidiian <meridian9695@mail.ru>
2024-03-03 18:56:40 +0300

    feat: update entities

--- Backend/RFLOT/RFLOT.Domain/Report/ValueObjects/EquipBadReport.cs
+++ Backend/RFLOT/RFLOT.Domain/Report/ValueObjects/EquipBadReport.cs
@@ -2,17 +2,21 @@
 
 public class EquipBadReport : ValueObject
 {
-    public EquipBadReport(string rfId, Status status)
+    public EquipBadReport(string rfId, Status status, string place)
     {
         RfId = rfId;
         Status = status;
+        Place = place;
     }
 
     public string RfId { get; private set; }
     public Status Status { get; private set; }
+    
+    public string Place { get; private set; }
     protected override IEnumerable<object> GetEqualityComponents()
     {
         yield return RfId;
         yield return Status;
+        yield return Place;
     }
 }
